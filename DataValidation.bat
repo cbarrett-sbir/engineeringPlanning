c64e786ba40cf5d9ede06cc5f408738e3b571417
Cameron Barrett <cameron.barrett@sbir.com>
2024-02-16 13:53:14 -0800

    first versions of batch scripts to run reports

---
+++ DataValidation.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Activate Conda environment
+call conda activate excel
+
+rem Change directory to the location of the Python script
+cd /d "Q:\EngineeringPlanning\ReportTools\stable builds\DataValidation"
+
+rem Start a new command prompt window and run the Python script in it
+start cmd /k "python main.py"
+
+rem Deactivate Conda environment (optional)
+call conda deactivate
